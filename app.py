327ff8a1b288e2d68f3fc055a8dbae0f62877ba7
jk <jk1124@gmail.com>
2024-04-16 10:20:52 +0800

    missing parameterrs handling

--- app.py
+++ app.py
@@ -25,10 +25,12 @@ def add(number1, number2):
 
 @app.route('/handle_url_params')
 def handle_params():
-    greeting = request.args['greeting']
-    name = request.args.get('name')
-    return f'{greeting}, {name}'
-
+    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+        greeting = request.args['greeting']
+        name = request.args.get('name')
+        return f'{greeting}, {name}'
+    else:
+        return 'Some parameters are missing'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555, debug=True)
