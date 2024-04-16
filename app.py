316979dd242ed29d2ada5ac5ac4c9d9da9ff73aa
jk <jk1124@gmail.com>
2024-04-16 11:53:53 +0800

    episode #3, initial remove previous codes

--- app.py
+++ app.py
@@ -8,34 +8,5 @@ def index():
     return "<h1>Hello World</h1>"
 
 
-@app.route('/hello')
-def hello():
-    response = make_response('Hello World\n')
-    response.status_code = 202
-    response.headers['content-type']="text/plain"
-    return response
-    
-
-
-@app.route('/greet/<name>')  #<name> is a variable
-def greet(name):
-    return f"Hello {name}"
-
-
-@app.route('/add/<int:number1>/<int:number2>')
-def add(number1, number2):
-    return f'{number1}+{number2}={number1+number2}'
-
-
-@app.route('/handle_url_params')
-def handle_params():
-    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
-        greeting = request.args['greeting']
-        name = request.args.get('name')
-        return f'{greeting}, {name}'
-    else:
-        return 'Some parameters are missing'
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5555, debug=True)
+    app.run(host='0.0.0.0', debug=True)
