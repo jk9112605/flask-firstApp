68001bce3f6fb7cca89a2e7d5a7a06174e697fd9
jk <jk1124@gmail.com>
2024-04-16 14:31:37 +0800

    demonstrate filter

--- app.py
+++ app.py
@@ -10,7 +10,20 @@ def index():
 
 @app.route('/other')
 def other():
-    return render_template('other.html')
+    some_text = "Hello World"
+    return render_template('other.html', some_text=some_text)
+
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1]
+
+@app.template_filter('repeat')
+def repeat(s, times=2):
+    return s*times
+
+@app.template_filter('alternate_case')
+def alternate_case(s):
+    return ''.join([c.upper() if i%2==0 else c.lower() for i, c in enumerate(s)])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
