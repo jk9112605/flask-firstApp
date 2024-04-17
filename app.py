c85ccd3e5890ea601a3c5e9a6413f98a30aaa4e1
jk <jk1124@gmail.com>
2024-04-17 08:57:45 +0800

    username and password file upload

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
- 
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        username = request.form.get(
+            'username')  #can use get(<varialbe name>) to get value
+        password = request.form[
+            'password']  #or can use form[<varialbe name>] to get value
+
+        if username == 'neuralnine' and password == 'password':
+            return 'Success'
+        else:
+            return 'Failure'
+
+
+@app.route('/file_upload', methods=['POST'])
+def file_upload():
+    file = request.files['file']
+    if file.content_type == 'text/plain':
+        return file.read().decode()
+    elif file.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or file.content_type == 'application/vnd.ms-excel':
+        return "this is excel file"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
